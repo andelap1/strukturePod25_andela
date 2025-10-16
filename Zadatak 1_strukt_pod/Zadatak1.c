48ffd6aad7d740bfcddaa9892a0efba5399cfdb9
andelap1 <apuljiz.puljiz@gmail.com>
2025-10-16 11:57:00 +0200

    Update Zadatak1.c

--- Zadatak 1_strukt_pod/Zadatak1.c
+++ Zadatak 1_strukt_pod/Zadatak1.c
@@ -6,6 +6,7 @@
 #define MAX_RED 1024
 #define MAX_BODOVI 100
 
+//definiramo strukturu student
 typedef struct student {
     char ime[50];
     char prezime[50];
@@ -13,9 +14,9 @@ typedef struct student {
 } Stud;
 
 
-int prebrojiRedke(const char* dat);
-Stud* ucitajStudente(const char* dat, int n);
-int ispisiStudente(Stud* studenti, int n);
+int prebrojiStudente(const char* dat);  //broji koliko studenata ima u datoteci
+Stud* ucitajStudente(const char* dat, int n); //ucitava podatke o studentima iz datoteke u niz struktura
+int ispisiStudente(Stud* studenti, int n); //ispis
 
 
 int main() {
@@ -23,31 +24,32 @@ int main() {
     printf("Unesite ime datoteke: ");
     scanf("%s", dat);
 
-    int broj = prebrojiRedke(dat);
+    int broj = prebrojiStudente(dat); //poziv funkcije koja broji studente, tj. redke
     if (broj <= 0) {
         printf("Greska: datoteka ne postoji ili je prazna!\n");
         return 1;
     }
 
-    Stud* studenti = ucitajStudente(dat, broj);
+    Stud* studenti = ucitajStudente(dat, broj); //ucitavanje studenata u dinamicki alociran niz jer ne znam unaprijed koliko ima studenata
     if (studenti == NULL) {
         printf("Greska pri ucitavanju studenata!\n");
         return 1;
     }
 
-    ispisiStudente(studenti, broj);
+    ispisiStudente(studenti, broj); 
 
-    free(studenti);
+    free(studenti); //oslobadjam memoriju koju sam zauzela mallocom
     return 0;
 }
 
 
-int prebrojiRedke(const char* dat) {
+int prebrojiStudente(const char* dat) {
     FILE* fp = fopen(dat, "r");
     if (fp == NULL) {
         return 0;
     }
 
+    //brojimo studente, tj. redove dok ih ima
     int brojac = 0;
     char buffer[MAX_RED];
     while (fgets(buffer, MAX_RED, fp) != NULL) {
@@ -60,9 +62,9 @@ int prebrojiRedke(const char* dat) {
 
 
 Stud* ucitajStudente(const char* dat, int n) {
-    Stud* studenti = (Stud*)malloc(n * sizeof(Stud));
+    Stud* studenti = (Stud*)malloc(n * sizeof(Stud)); //dinamicki alociramo memoriju za n broj studenata
     if (studenti == NULL) {
-        return NULL;
+        return NULL; //ovo ako nema dovoljno memorije
     }
 
     FILE* fp = fopen(dat, "r");
@@ -71,6 +73,7 @@ Stud* ucitajStudente(const char* dat, int n) {
         return NULL;
     }
 
+    //ucitavam svakog studenta iz datoteke red po red
     char buffer[MAX_RED];
     int i = 0;
     while (fgets(buffer, MAX_RED, fp) != NULL && i < n) {
@@ -83,22 +86,23 @@ Stud* ucitajStudente(const char* dat, int n) {
     }
 
     fclose(fp);
-    return studenti;
+    return studenti; //pokazivac vracam na niz studenata
 }
 
 int ispisiStudente(Stud* studenti, int n) {
     printf("\n%-15s %-15s %-15s %-15s\n", "IME", "PREZIME", "APS. BODOVI", "REL. BODOVI (%)");
-    printf("---------------------------------------------------------------\n");
+    printf("---------------------------------------------------------------\n"); //zaglavlje tablice
 
     for (int i = 0; i < n; i++) {
-        double rel = (studenti[i].bodovi / MAX_BODOVI) * 100.0;
+        double rel_bod = (studenti[i].bodovi / MAX_BODOVI) * 100.0;
         printf("%-15s %-15s %-15.2lf %-15.2lf\n",
             studenti[i].ime,
             studenti[i].prezime,
             studenti[i].bodovi,
-            rel);
+            rel_bod);
     }
 
     return;
 }
+
 
